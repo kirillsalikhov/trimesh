29de3d52ee97bcdbe552bb148f6db035a8e84a85
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-08 16:55:58 +0000

    Deploy mikedh/trimesh to github.com/mikedh/trimesh.git:gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.30.39',
+    VERSION: '2.30.40',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
